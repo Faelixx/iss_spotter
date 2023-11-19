dd9e53f66e6f48c368966cf47b8a0adbd4d546cb
Faelixx <sasha.taylor7@gmail.com>
2023-11-19 15:05:01 -0700

    Feat: Retrieve geographical location by IP address

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+// fetchCoordsByIP(fetchMyIP((error, ip)=> {
+//   if (error) {
+//     console.log("It didnt work!", error);
+//     return;
+//   }
+//   console.log("It worked! Returned IP:", ip);
+//   return ip;
+// }) , (error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+fetchCoordsByIP("68.147.86.233", (error, ip) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
-
   console.log('It worked! Returned IP:', ip);
-})
+});
--- iss.js
+++ iss.js
@@ -11,12 +11,47 @@ const request = require('request');
 
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, message, body) => {
-    if(!error && message.statusCode === 200) {
-      callback(null, body);
+    if (!error && message.statusCode === 200) {
+      const data = JSON.parse(body);
+      const tickedIP = JSON.stringify(data.ip);
+      const splitIP = tickedIP.split('');
+      splitIP.pop();
+      splitIP.shift();
+      const IP = splitIP.join('');
+      callback(null, IP);
+      return IP;
+    } else if (message.statusCode !== 200) {
+      const msg = `Status Code ${message.statusCode} when fetching IP. Response: ${body}`;
+      callback(error(msg), null);
+      return;
     } else {
       callback(error, null);
     }
-  })
-}
+  });
+};
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(IP, callback) {
+  const geoURL = "https://ipwho.is/";
+  request(`${geoURL}${IP}`, (error, message, body) =>{
+    if (!error && message.statusCode === 200) {
+      const data = JSON.parse(body);
+      const latLong = { 
+        "latitude" : data["latitude"], 
+        "longitiude" : data["longitude"] };
+      console.log(latLong);
+    }
+    if (error) {
+      console.log("You have an error: ", error)
+    }
+    if (message.statusCode !== 200) {
+      console.log("There is something wrong here! Check the code: ", message.statusCode);
+    }
+  });
+  callback(null, IP);
+};
+
+
+module.exports = { 
+  fetchMyIP,
+  fetchCoordsByIP
+  };
